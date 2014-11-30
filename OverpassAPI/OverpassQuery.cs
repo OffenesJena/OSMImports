8968f77d8b382c8c2e51240c6dd54dd1e4912ef7
Achim Friedland <github@ahzf.de>
2014-11-30 14:06:18 +0100

    Allow to narrow down queries via AND. Allow to set the timeout of a query.

--- OverpassAPI/OverpassQuery.cs
+++ OverpassAPI/OverpassQuery.cs
@@ -39,9 +39,6 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
     public class OverpassQuery
     {
 
-        private        Object   LockObject  = new Object();
-        private static DateTime LastCall    = DateTime.Now;
-
         #region Documentation
 
         // A Overpass query example...
@@ -65,17 +62,19 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
         /// <summary>
         /// The URI of the OverpassAPI.
         /// </summary>
-        public static readonly Uri          OverpassAPI_URI  = new Uri("http://overpass-api.de/api/interpreter");
+        public static readonly Uri                  OverpassAPI_URI  = new Uri("http://overpass-api.de/api/interpreter");
 
         /// <summary>
-        /// The URIO of the NominatimAPI.
+        /// The URI of the NominatimAPI.
         /// </summary>
-        public static readonly Uri          NominatimAPI_URI = new Uri("http://nominatim.openstreetmap.org/search");
+        public static readonly Uri                  NominatimAPI_URI = new Uri("http://nominatim.openstreetmap.org/search");
 
 
-        private Dictionary<String, String>  Nodes;
-        private Dictionary<String, String>  Ways;
-        private Dictionary<String, String>  Relations;
+        private QueryContext                        CurrentContext;
+
+        private List<KeyValuePair<String, String>>  Nodes;
+        private List<KeyValuePair<String, String>>  Ways;
+        private List<KeyValuePair<String, String>>  Relations;
 
         #endregion
 
@@ -98,6 +97,60 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
 
         #endregion
 
+        #region QueryTimeout
+
+        private UInt32 _QueryTimeout = 100;
+
+        /// <summary>
+        /// The timeout of the query.
+        /// </summary>
+        public UInt32 QueryTimeout
+        {
+            get
+            {
+                return _QueryTimeout;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region (private, enum) QueryContext
+
+        /// <summary>
+        /// The current query state which allows to narrow down a query using AND.
+        /// </summary>
+        private enum QueryContext
+        {
+
+            /// <summary>
+            /// Illegal state.
+            /// </summary>
+            none,
+
+            /// <summary>
+            /// Query nodes.
+            /// </summary>
+            Nodes,
+
+            /// <summary>
+            /// Query ways.
+            /// </summary>
+            Ways,
+
+            /// <summary>
+            /// Query relations.
+            /// </summary>
+            Relations,
+
+            /// <summary>
+            /// Query nodes, ways and relations.
+            /// </summary>
+            Any
+
+        }
+
         #endregion
 
         #region Constructor(s)
@@ -110,11 +163,13 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
         public OverpassQuery()
         {
 
-            this._AreaId     = 0;
+            _AreaId         = 0;
 
-            this.Nodes      = new Dictionary<String, String>();
-            this.Ways       = new Dictionary<String, String>();
-            this.Relations  = new Dictionary<String, String>();
+            CurrentContext  = QueryContext.none;
+
+            Nodes           = new List<KeyValuePair<String, String>>();
+            Ways            = new List<KeyValuePair<String, String>>();
+            Relations       = new List<KeyValuePair<String, String>>();
 
         }
 
@@ -257,12 +312,19 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
 
         #region WithAny(Type, Value = "")
 
+        /// <summary>
+        /// Query nodes, ways and relations.
+        /// </summary>
+        /// <param name="Type">The key to search for.</param>
+        /// <param name="Value">The value to search for.</param>
         public OverpassQuery WithAny(String Type, String Value = "")
         {
 
-            this.Nodes.    Add(Type, Value);
-            this.Ways.     Add(Type, Value);
-            this.Relations.Add(Type, Value);
+            Nodes.    Add(new KeyValuePair<String, String>(Type, Value));
+            Ways.     Add(new KeyValuePair<String, String>(Type, Value));
+            Relations.Add(new KeyValuePair<String, String>(Type, Value));
+
+            CurrentContext = QueryContext.Any;
 
             return this;
 
@@ -272,63 +334,122 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
 
         #region WithNodes(NodeType, Value = "")
 
+        /// <summary>
+        /// Query nodes.
+        /// </summary>
+        /// <param name="NodeType">The key to search for.</param>
+        /// <param name="Value">The value to search for.</param>
         public OverpassQuery WithNodes(String NodeType, String Value = "")
         {
-            this.Nodes.Add(NodeType, Value);
+
+            Nodes.Add(new KeyValuePair<String, String>(NodeType, Value));
+
+            CurrentContext = QueryContext.Nodes;
+
             return this;
+
         }
 
         #endregion
 
-        #region WithWays(NodeType, Value = "")
+        #region WithWays(WayType, Value = "")
 
+        /// <summary>
+        /// Query OSM ways.
+        /// </summary>
+        /// <param name="WayType">The key to search for.</param>
+        /// <param name="Value">The value to search for.</param>
         public OverpassQuery WithWays(String WayType, String Value = "")
         {
-            this.Ways.Add(WayType, Value);
+
+            Ways.Add(new KeyValuePair<String, String>(WayType, Value));
+
+            CurrentContext = QueryContext.Ways;
+
             return this;
+
         }
 
         #endregion
 
         #region WithRelations(RelationType, Value = "")
 
+        /// <summary>
+        /// Query OSM relations.
+        /// </summary>
+        /// <param name="RelationType">The key to search for.</param>
+        /// <param name="Value">The value to search for.</param>
         public OverpassQuery WithRelations(String RelationType, String Value = "")
         {
-            this.Relations.Add(RelationType, Value);
+
+            Relations.Add(new KeyValuePair<String, String>(RelationType, Value));
+
+            CurrentContext = QueryContext.Relations;
+
             return this;
+
+        }
+
+        #endregion
+
+        #region And(Type, Value = "")
+
+        /// <summary>
+        /// Query the current context. This allows you to chain a query via AND.
+        /// </summary>
+        /// <param name="Type">The key to search for.</param>
+        /// <param name="Value">The value to search for.</param>
+        public OverpassQuery And(String Type, String Value = "")
+        {
+
+            switch (CurrentContext)
+            {
+
+                case QueryContext.none:         throw new Exception("Bad request!");
+
+                case QueryContext.Any:          WithAny      (Type, Value); break;
+                case QueryContext.Nodes:        WithNodes    (Type, Value); break;
+                case QueryContext.Ways:         WithWays     (Type, Value); break;
+                case QueryContext.Relations:    WithRelations(Type, Value); break;
+
+            }
+
+            return this;
+
         }
 
         #endregion
 
 
-        #region RunQuery()
+        #region SetTimeout(Timeout)
+
+        /// <summary>
+        /// Set the query timeout.
+        /// </summary>
+        /// <param name="Timeout">The timeout value.</param>
+        public OverpassQuery SetTimeout(UInt32 Timeout)
+        {
+
+            if (Timeout > 0)
+                _QueryTimeout = Timeout;
+
+            return this;
+
+        }
+
+        #endregion
+
+        #region RunQuery(Timeout = 0)
 
         /// <summary>
         /// Execute this Overpass query.
         /// </summary>
         /// <returns>A Overpass query result.</returns>
-        public async Task<OverpassResult> RunQuery()
+        public async Task<OverpassResult> RunQuery(UInt32 Timeout = 0)
         {
 
-            var wait = false;
-
-            lock (LockObject)
-            {
-
-                do
-                {
-
-                    var CurrentTime = DateTime.Now;
-
-                    if (CurrentTime - LastCall < TimeSpan.FromSeconds(2))
-                        Thread.Sleep(500);
-
-                    else
-                        wait = false;
-
-                } while (wait);
-
-            }
+            if (Timeout > 0)
+                _QueryTimeout = Timeout;
 
             using (var HTTPClient = new HttpClient())
             {
@@ -458,6 +579,26 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
         #endregion
 
 
+        #region (private) FormatQuery(Type, Collection)
+
+        /// <summary>
+        /// Format the given query based on its type and collection.
+        /// </summary>
+        /// <param name="Type">The type of the query (node, way, relation).</param>
+        /// <param name="Collection">The collection of query items.</param>
+        private String FormatQuery(String                                     Type,
+                                   IEnumerable<KeyValuePair<String, String>>  Collection)
+        {
+
+            return String.Concat(Type,
+                                 Collection.Select(Item => String.Concat(@"[""", Item.Key, @"""", (Item.Value != "" ? @"=""" + Item.Value + @"""" : ""), "]")).
+                                            Aggregate((a, b) => a + b),
+                                 (_AreaId > 0 ? " (area.searchArea);" : ";"));
+
+        }
+
+        #endregion
+
         #region ToString()
 
         /// <summary>
@@ -469,21 +610,21 @@ namespace org.GraphDefined.OpenDataAPI.OverpassAPI
             var QueryString = new StringBuilder();
 
             QueryString.AppendLine("[out:json]");
-            QueryString.AppendLine("[timeout:100];");
+            QueryString.AppendLine("[timeout:" + _QueryTimeout + "];");
 
             if (_AreaId > 0)
                 QueryString.AppendLine("area(" + _AreaId.ToString() + ")->.searchArea;");
 
             QueryString.AppendLine("(");
 
-            foreach (var Node in Nodes)
-                QueryString.AppendLine(String.Concat(@"node[""", Node.Key, @"""", (Node.Value != "" ? @"=""" + Node.Value + @"""" : ""), "]", (_AreaId > 0 ? " (area.searchArea);" : ";")));
+            if (Nodes.Count > 0)
+                QueryString.AppendLine(FormatQuery("node",     Nodes));
 
-            foreach (var Way in Ways)
-                QueryString.AppendLine(String.Concat(@"way[""", Way.Key, @"""", (Way.Value != "" ? @"=""" + Way.Value + @"""" : ""), "]", (_AreaId > 0 ? " (area.searchArea);" : ";")));
+            if (Ways.Count > 0)
+                QueryString.AppendLine(FormatQuery("way",      Ways));
 
-            foreach (var Relation in Relations)
-                QueryString.AppendLine(String.Concat(@"relation[""", Relation.Key, @"""", (Relation.Value != "" ? @"=""" + Relation.Value + @"""" : ""), "]", (_AreaId > 0 ? " (area.searchArea);" : ";")));
+            if (Relations.Count > 0)
+                QueryString.AppendLine(FormatQuery("relation", Relations));
 
             QueryString.AppendLine(");");
 
